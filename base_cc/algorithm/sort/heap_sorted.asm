0830929009da21a02d084659891a8dd642210860
v_fantnzeng <v_fantnzeng@tencent.com>
2021-03-26 10:46:46 +0800

    1

---
+++ base_cc/algorithm/sort/heap_sorted.asm
@@ -0,0 +1,27 @@
+	.file	"heap_sorted.cc"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1969:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1969:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
